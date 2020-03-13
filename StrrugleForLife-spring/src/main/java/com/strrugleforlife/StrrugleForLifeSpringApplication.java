e281d8fcfa020dcb1de0b0604a224ceba713e10d
strrugleforlife <kuro.y6222@gmail.com>
2020-03-13 21:40:32 +0900

    test

---
+++ StrrugleForLife-spring/src/main/java/com/strrugleforlife/StrrugleForLifeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.strrugleforlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrrugleForLifeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrrugleForLifeSpringApplication.class, args);
+	}
+
+}
---
+++ StrrugleForLife-spring/src/test/java/com/strrugleforlife/StrrugleForLifeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strrugleforlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrrugleForLifeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
